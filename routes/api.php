5d0d8bf60822b6c314d69f62225af166b2f3885c
Frank Sierra <sierrafayad@gmail.com>
2019-09-04 17:59:46 -0500

    Prep Laravel 6.0 🔥

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
-//Non Auth Request
