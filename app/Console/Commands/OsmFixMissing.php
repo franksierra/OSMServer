a8badcea8d5b6f65fd37a3218282b7b6508b97dd
Frank Sierra <sierrafayad@gmail.com>
2019-09-11 18:15:21 -0500

    wip: fixing missing ways

--- app/Console/Commands/OsmFixMissing.php
+++ app/Console/Commands/OsmFixMissing.php
@@ -3,18 +3,11 @@
 namespace App\Console\Commands;
 
 use App\Geo\OSM;
-use App\Models\OSM\Node;
-use App\Models\OSM\NodeTag;
-use App\Models\OSM\Relation;
-use App\Models\OSM\RelationMember;
 use App\Models\OSM\RelationTag;
-use App\Models\OSM\Way;
-use App\Models\OSM\WayNode;
-use App\Models\OSM\WayTag;
+use GuzzleHttp\Client;
 use Illuminate\Console\Command;
-
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Support\Facades\Storage;
-use OSMPBF\OSMReader;
 
 class OsmFixMissing extends Command
 {
@@ -32,6 +25,10 @@ class OsmFixMissing extends Command
      */
     protected $description = 'Imports an OSM file into the database';
 
+    private $cache_dir = 'OsmFixCache/';
+
+    private $guzzle_client = null;
+
     /**
      * Create a new command instance.
      *
@@ -40,6 +37,8 @@ class OsmFixMissing extends Command
     public function __construct()
     {
         parent::__construct();
+
+        $this->guzzle_client = new Client();
     }
 
     /**
@@ -49,15 +48,60 @@ class OsmFixMissing extends Command
      */
     public function handle()
     {
+        Storage::disk('local')->makeDirectory($this->cache_dir);
         $territories = RelationTag::where('k', '=', 'admin_level')
             ->whereIn('v', [2])
             ->orderBy('relation_id', 'ASC')->get();
         foreach ($territories as $territory) {
             $geometry = OSM::relationGeometry($territory->relation->id);
-
-
+            foreach ($geometry->empty_ways as $id => $nil) {
+                $data = $this->getWay($id);
+                $this->fixWay($data);
+            }
         }
 
         return true;
+    }
+
+    private function getWay($id)
+    {
+        $file = $this->cache_dir . 'way-' . $id . '.xml';
+        if (!Storage::exists($file)) {
+            $api_url = "https://www.openstreetmap.org/api/0.6/way/" . $id . "/full";
+            $result = $this->guzzle_client->get($api_url, ['verify' => false]);
+            $xml = (string)$result->getBody();
+            Storage::append($file, $xml);
+        } else {
+            try {
+                $xml = Storage::get($file);
+            } catch (FileNotFoundException $e) {
+                return false;
+            }
+        }
+
+        return $xml;
+    }
+
+    private function fixWay($data)
+    {
+        $xml = simplexml_load_string($data);
+        if ($xml == FALSE) {
+            return false;
+        }
+        $full = json_decode(json_encode($xml), TRUE);
+//
+//        foreach ($json->node as $item) {
+//            $data = $this->proccess($item);
+//            $this->makeEntity('node', $data);
+//        }
+//        if (!is_array($json->way)) {
+//            $data = $this->proccess($json->way);
+//            $this->makeEntity('way', $data);
+//        } else {
+//            foreach ($json->way as $item) {
+//                $data = $this->proccess($item);
+//                $this->makeEntity('way', $data);
+//            }
+//        }
     }
 }
